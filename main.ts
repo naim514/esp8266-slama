f29e4bba8e58a2f933fc10fb1cd21dc8f5191b71
naim <96814060+naim514@users.noreply.github.com>
2022-04-15 15:32:54 +0100

    Mise à jour main.blocks, main.ts, esp8266-slama.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(somme(1, 1))
 })
+function somme (num: number, num2: number) {
+    return num + num2
+}
